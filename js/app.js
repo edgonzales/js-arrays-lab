a0e89f09fab4da99855e9133c74971b585eb1f23
Ed Gonzales <edgonzales6@gmail.com>
2023-09-27 08:53:07 -0700

    Completed exercises 1 and 2

--- js/app.js
+++ js/app.js
@@ -1,2 +1,21 @@
+/*
+Exercise 1:
+  - Define an empty array named foods
+*/
 
-console.log('My app.js is being run!');
+// Exercise 1 has been completed for you...
+
+const foods = [];
+
+console.log('Exercise 1 Result:\n', foods);
+
+/*
+Exercise 2:
+  - Add the strings 'pizza' & 'cheeseburger' to the foods array such that 'pizza' comes before 'cheeseburger'.
+*/
+
+// Complete Exercise 2 below...
+// Adding pizza and cheeseburger to the array, in that order.
+foods.push('pizza', 'cheeseburger');
+
+console.log('Exercise 2 Result:\n',  foods);
